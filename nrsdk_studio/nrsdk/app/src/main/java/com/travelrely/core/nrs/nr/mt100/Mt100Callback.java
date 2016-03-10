d26f921676bae7ef5b8d5eff3eee3debf829b672
cuiwj <oldcwj@gmail.com>
2016-03-10 15:17:17 +0800

    remove nrsdk_studio

--- nrsdk_studio/nrsdk/app/src/main/java/com/travelrely/core/nrs/nr/mt100/Mt100Callback.java
+++
@@ -1,7 +0,0 @@
-package com.travelrely.core.nrs.nr.mt100;
-
-public interface Mt100Callback
-{
-    public void callRslt(byte[] rslt);
-    public void sendBtMsg(byte[] msg);
-}
--- nrsdk_studio/nrsdk/app/src/main/java/com/travelrely/model/Model.java
+++
@@ -1,5 +0,0 @@
-package com.travelrely.model;
-
-public class Model {
-
-}
