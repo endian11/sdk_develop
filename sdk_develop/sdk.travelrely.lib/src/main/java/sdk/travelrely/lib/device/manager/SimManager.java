9ef56cb6ad3e6947e95798193eaa9d099e2ac91d
endian11 <Dana_Lee1016@126.com>
2016-03-10 15:09:04 +0800

    提交simManager

--- sdk_develop/sdk.travelrely.lib/src/main/java/sdk/travelrely/lib/device/manager/SimManager.java
+++ sdk_develop/sdk.travelrely.lib/src/main/java/sdk/travelrely/lib/device/manager/SimManager.java
@@ -1,14 +1,9 @@
 package sdk.travelrely.lib.device.manager;
 
 import android.os.Message;
-import android.text.TextUtils;
 
 import sdk.travelrely.lib.Constant;
-import sdk.travelrely.lib.TRAction;
 import sdk.travelrely.lib.device.exception.BLEException;
-import sdk.travelrely.lib.device.util.BLEUtil;
-import sdk.travelrely.lib.util.LogUtil;
-import sdk.travelrely.lib.util.SharedUtil;
 
 /**
  * Created by john on 2016/3/9.
@@ -21,6 +16,10 @@ public class SimManager extends BaseManager {
         super();
     }
 
+
+    public static final int ACTION_READ_SIM = 0;
+    public static  int CURRENT_ACTION = -1;
+
     public static SimManager getDefault() {
         return manager;
     }
@@ -30,30 +29,23 @@ public class SimManager extends BaseManager {
         super.receiveMessage(message);
 
         switch (message.what) {
-            case BoxManager.ACTION_CHECK_KEY:
-                LogUtil.d("receive BoxManager.ACTION_CHECK_KEY :" + message.obj);
-                break;
+
+            case ACTION_READ_SIM:
+                //收到SIM卡信息解析完之后的数据
+                               break;
         }
     }
 
-
+    /**
+     * 读取sim卡信息
+     */
     public void readSimInfo() {
-
-        if (BLEManager.getDefault().isConnect()) {
-            //蓝牙已经连接 校验key
-            BoxManager.getDefault().sendChkKey();
-
-            //校验key，失败 。整个任务失败
-        } else {
-            //蓝牙未连接，则连接蓝牙
-//            String mac = SharedUtil.get(TRAction.SHARED_BT_ADDR, "");
-//            if (!TextUtils.isEmpty(mac)) {
-//                BLEManager.getDefault().connect(mac);
-//
-//            } else {
-//                LogUtil.d("读取sim卡信息失败");
-//            }
-
+        CURRENT_ACTION = ACTION_READ_SIM;
+        try {
+            BLEManager.getDefault().send(Constant.simInfoReq);
+        } catch (BLEException e) {
+            e.printStackTrace();
         }
     }
+
 }
